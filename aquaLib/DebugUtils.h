26480621c479be0bec9be4742c0aa629f12aa468
VirtualJabberWock <loopinyou12@gmail.com>
2022-10-19 23:03:46 +0500

    Init
    init

---
+++ aquaLib/DebugUtils.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <stdio.h>
+#include <stdlib.h>
+#define string const char*
+
+void panic(string msg);
+void h1dd3n_p4n1c();
+int panic_e(string msg);
+void printIntArray(int* array, int len);
+void setLastError(int channel, string value);
+string getLastError(int channel);
+void setTemp(int channel, string value);
+string getTemp(int channel);
---
+++ pch.h
@@ -0,0 +1,6 @@
+#pragma once
+#include "aquaLib/DynamicList.h"
+#include "aquaLib/DebugUtils.h"
+#include "aquaLib/FileUtils.h"
+#include "aquaLib/StringUtils.h"
+#include "aquaLib/Storage.h"
