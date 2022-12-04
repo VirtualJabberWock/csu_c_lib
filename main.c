4de22e9e0efb49e598e99dd1c6da96cfe82e9be2
VirtualJabberWock <115100398+VirtualJabberWock@users.noreply.github.com>
2022-12-04 20:35:47 +0500

    04_12_2022_20_35
    04_12_2022_20_35

--- main.c
+++ main.c
@@ -3,12 +3,9 @@
 #include "aquaUtils/BasicDataStructs.h"
 
 int main() {
-	HashMap* hmap = NewHashMap();
-	hmap->put(hmap, "name", "Aqua");
-	hmap->put(hmap, "surname", "Haze");
-	hmap->put(hmap, "number", "+79525160897");
-	hmap->put(hmap, "Abc", "Collision Check!");
-	hmap->put(hmap, "abc", "Collision Check!?");
-	DataBox* box = DissectHashMap(hmap);
-	box->display(box);
+	int j = 0;
+	for (int i = 0; i < 1; i++) {
+		printf("current = %d", j);
+		j++;
+	}
 }
